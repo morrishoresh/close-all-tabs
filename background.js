68406f6f563d53a6cbad923dc438b4b611e30a54
Morris Horesh <morris_horesh@yahoo.com>
2026-09-06 01:14:37 +0200

    Close only tabs in this window

--- background.js
+++ background.js
@@ -1,16 +1,10 @@
-console.log("Close All Tabs: background script loaded");
-
 browser.action.onClicked.addListener(async () => {
-  console.log("Close All Tabs: button clicked");
-
-  const oldTabs = await browser.tabs.query({});
+  const oldTabs = await browser.tabs.query({ currentWindow: true });
   const oldIds = oldTabs.map((t) => t.id);
-  console.log("Close All Tabs: found tabs", oldIds);
 
   let homeUrl = null;
   try {
     const setting = await browser.browserSettings.homepageOverride.get({});
-    console.log("Close All Tabs: homepageOverride", setting);
     if (setting && setting.value) {
       const firstUrl = setting.value.split("|")[0].trim();
       if (firstUrl && !firstUrl.startsWith("about:")) {
@@ -18,24 +12,19 @@ browser.action.onClicked.addListener(async () => {
       }
     }
   } catch (e) {
-    console.log("Close All Tabs: homepageOverride read failed", e);
+    // Fall back to default new tab if the setting can't be read.
   }
 
-  try {
-    if (homeUrl) {
-      await browser.tabs.create({ url: homeUrl });
-    } else {
-      // Default new tab page (Firefox Home) - extensions can't open
-      // about:home directly, so omit the url to get the same result.
-      await browser.tabs.create({});
-    }
-    console.log("Close All Tabs: new tab created", homeUrl || "(default new tab)");
-  } catch (e) {
-    console.log("Close All Tabs: tab creation failed", e);
+  if (homeUrl) {
+    await browser.tabs.create({ url: homeUrl });
+  } else {
+    // Default new tab page (Firefox Home) - extensions can't open
+    // about:home directly, so omit the url to get the same result.
+    await browser.tabs.create({});
   }
 
   if (oldIds.length > 0) {
     await browser.tabs.remove(oldIds);
-    console.log("Close All Tabs: old tabs removed");
   }
 });
+
